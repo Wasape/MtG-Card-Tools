458b5ed153425123b45f9167956911754fe90479
Kerim Strikovic <kerim.strikovic@gmail.com>
2022-07-06 08:23:16 -0500

    Add source data and un-comment code. KS

--- Card Search.py
+++ Card Search.py
@@ -28,52 +28,52 @@ reqCount = len(reqResults)
 print(reqCount)
 
 
-##READ LOCAL MTGJSON FILE AND FILTER INVALID ELEMENTS
+#READ LOCAL MTGJSON FILE AND FILTER INVALID ELEMENTS
 
-##### place the path to your local mtg JSON file in the next line
+#### place the path to your local mtg JSON file in the next line
 
-# with open(r"#####your-local-file-is-here#####", "r", encoding='utf-8') as read_file:
-    # rawData = json.load(read_file)
+with open(r"AtomicCards.json", "r", encoding='utf-8') as read_file:
+    rawData = json.load(read_file)
 
-# rawCardData = dict(rawData['data'])
-# cardDict = {}
+rawCardData = dict(rawData['data'])
+cardDict = {}
 
-# for c in list(rawCardData.keys()):
-#     try:
-#         if not rawCardData[c][0]['legalities']['vintage']:
-#             continue
-#     except KeyError:
-#         continue
-#     else:
-#         cardDict[c] = c
+for c in list(rawCardData.keys()):
+    try:
+        if not rawCardData[c][0]['legalities']['vintage']:
+            continue
+    except KeyError:
+        continue
+    else:
+        cardDict[c] = c
 
-# cardNames = list(cardDict.keys())
-
-
-
-## SEARCH THROUGH MODIFIED LOCAL LIST
-
-# myResults = []
-# myCount = 0
-
-# for n in cardNames:
-    # if searchTerm.lower() in n.lower():
-        # myResults.append(n)
-        # myCount+=1
-
-# print(myCount)
+cardNames = list(cardDict.keys())
 
 
 
-## COMPARE THE SEARCH RESULTS
+# SEARCH THROUGH MODIFIED LOCAL LIST
 
-##### may need to switch the order depending on which is larger
+myResults = []
+myCount = 0
 
-# for r in reqResults:
-    # if r not in myResults:
-        # print(r)
+for n in cardNames:
+    if searchTerm.lower() in n.lower():
+        myResults.append(n)
+        myCount+=1
+
+print(myCount)
 
 
-## TO VIEW SPECIFIC RAW CARD DATA
 
-# print(rawCardData['#####Card Name Here#####'])
+# COMPARE THE SEARCH RESULTS
+
+#### may need to switch the order depending on which is larger
+
+for r in reqResults:
+    if r not in myResults:
+        print(r)
+
+
+# TO VIEW SPECIFIC RAW CARD DATA
+
+print(rawCardData['#####Card Name Here#####'])

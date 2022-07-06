28fdf6dacd3a293534f8eb85158f07acc38b00cd
Wasape <108776171+Wasape@users.noreply.github.com>
2022-07-05 21:55:11 -0500

    Update Card Search.py

--- Card Search.py
+++ Card Search.py
@@ -1,7 +1,9 @@
 import json
+import requests
+
 
 ## GET NAME SEARCH RESULTS FROM SCRYFALL IN LIST FORM
-import requests
+
 api_url = "https://api.scryfall.com/cards/search?q=the+%28-is%3Adigital+-is%3Afunny+-is%3Apromo%29"
 response =  requests.get(api_url)
 responseData = response.json()['data']
@@ -13,58 +15,62 @@ while response.json()['has_more']:
     responseData += responseDataNext
     response = responseNext
 
-resultsList = []
+reqResults = []
 for item in responseData:
     name = item['name']
-    resultsList.append(item['name'])
+    reqResults.append(item['name'])
 
-print(len(resultsList))
+print(len(reqResults))
 
 
 
 ##READ LOCAL MTGJSON FILE AND FILTER INVALID ELEMENTS
 
-with open(r"C:\Users\Jeremy\Desktop\JEREMY\Python Files\AtomicCards.json", "r", encoding='utf-8') as read_file:
-    rawData = json.load(read_file)
+##### place the path to your local mtg JSON file in the next line
 
-rawCardData = dict(rawData['data'])
-cardDict = {}
+# with open(r"#####your-local-file-is-here#####", "r", encoding='utf-8') as read_file:
+    # rawData = json.load(read_file)
 
-for c in list(rawCardData.keys()):
-    if rawCardData[c][0]['types'][0] == 'Vanguard':
-        continue
-    try:
-        if rawCardData[c][0]['legalities'] == {}:
-            continue
-    except KeyError:
-        continue
-    else:
-        cardDict[c] = c
+# rawCardData = dict(rawData['data'])
+# cardDict = {}
 
+# for c in list(rawCardData.keys()):
+    # if rawCardData[c][0]['types'][0] == 'Vanguard':
+        # continue
+    # try:
+        # if rawCardData[c][0]['legalities'] == {}:
+            # continue
+    # except KeyError:
+        # continue
+    # else:
+        # cardDict[c] = c
 
-cardNames = list(cardDict.keys())
+# cardNames = list(cardDict.keys())
 
 
 
 ## SEARCH THROUGH MODIFIED LOCAL LIST
 
-searchTerm = 'the'
-results = []
-count = 0
+# searchTerm = 'the'
+# myResults = []
+# count = 0
 
-for n in cardNames:
-    if searchTerm in n.lower():
-        results.append(n)
-        count+=1
+# for n in cardNames:
+    # if searchTerm in n.lower():
+        # results.append(n)
+        # count+=1
 
-print(count)
+# print(count)
 
 
 
 ## COMPARE THE SEARCH RESULTS
 
-for r in resultsList:
-    if r not in results:
-        print(r)
+# for r in resultsList:
+    # if r not in results:
+        # print(r)
 
-print(rawCardData['Brisela, Voice of Nightmares'])
+
+## TO VIEW SPECIFIC RAW CARD DATA
+
+# print(rawCardData['Brisela, Voice of Nightmares'])
